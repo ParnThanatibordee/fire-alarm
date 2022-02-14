093f9261b3b60dff4867cfe87c92f7416b48c901
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2022-02-14 14:24:40 +0700

    done signup page frame

--- fire-alarm-back-end/src/components/Login.js
+++ fire-alarm-back-end/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,27 +6,27 @@ const Login = () => {
   return (
     <div className='loginPage'>
          <div id="login">
-        <h3 class="text-center text-white pt-5">Fire ALert</h3>
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info">Login</h3>
-                            <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br/>
-                                <input type="text" name="username" id="username" class="form-control" />
+        <h3 className="text-center text-white pt-5">Fire ALert</h3>
+        <div className="container">
+            <div id="login-row" className="row justify-content-center align-items-center">
+                <div id="login-column" className="col-md-6">
+                    <div id="login-box" className="col-md-12">
+                        <form id="login-form" className="form" action="" method="post">
+                            <h3 className="text-center text-info">Login</h3>
+                            <div className="form-group">
+                                <label for="username" className="text-info">Username:</label><br/>
+                                <input type="text" name="username" id="username" className="form-control" />
                             </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br/>
-                                <input type="text" name="password" id="password" class="form-control" />
+                            <div className="form-group">
+                                <label for="password" className="text-info">Password:</label><br/>
+                                <input type="text" name="password" id="password" className="form-control" />
                             </div>
-                            <div class="form-group">
-                                <label for="remember-me" class="text-info"><br/><span>Remember me</span><span><input id="remember-me" name="remember-me" type="checkbox"/></span></label><br/>
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit"/>
+                            <div className="form-group">
+                                <label for="remember-me" className="text-info"><br/><span>Remember me</span><span><input id="remember-me" name="remember-me" type="checkbox"/></span></label><br/>
+                                <input type="submit" name="submit" className="btn btn-info btn-md" value="submit"/>
                             </div>
-                            <div id="register-link" class="text-right">
-                                <Link to="/signup" class="text-info">Register here</Link>
+                            <div id="register-link" className="text-right">
+                                <Link to="/signup" className="text-info">Register here</Link>
                             </div>
                         </form>
                     </div>
--- fire-alarm-back-end/src/components/SignUp.js
+++ fire-alarm-back-end/src/components/SignUp.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className="row centered-form">
+        <div className="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h3 className="panel-title">Fire Alart Sign UP</h3>
+                </div>
+                <div className="panel-body">
+                    <form role="form">
+                        <div className="row">
+                            <div className="col-xs-6 col-sm-6 col-md-6">
+                                <div className="form-group">
+                                    <input type="text" name="first_name" id="first_name" className="form-control input-sm" placeholder="First Name"  />
+                                </div>
+                            </div>
+                            <div className="col-xs-6 col-sm-6 col-md-6">
+                                <div className="form-group">
+                                    <input type="text" name="last_name" id="last_name" className="form-control input-sm" placeholder="Last Name" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="email" name="email" id="email" className="form-control input-sm" placeholder="Email Address" />
+                        </div>
+
+                        <div className="row">
+                            <div className="col-xs-6 col-sm-6 col-md-6">
+                                <div className="form-group">
+                                    <input type="password" name="password" id="password" className="form-control input-sm" placeholder="Password" />
+                                </div>
+                            </div>
+                            <div className="col-xs-6 col-sm-6 col-md-6">
+                                <div className="form-group">
+                                    <input type="password" name="password_confirmation" id="password_confirmation" className="form-control input-sm" placeholder="Confirm Password" />
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <input type="submit" value="Register" className="btn btn-info btn-block" />
+                    
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
