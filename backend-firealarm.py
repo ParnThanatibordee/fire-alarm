5dff8ae1540d50fc3edaebcb01abf8ab911780fc
Kop <copter1144@hotmail.com>
2022-02-16 15:09:27 +0700

    Update backend-firealarm.py
    update record

--- backend-firealarm.py
+++ backend-firealarm.py
@@ -31,16 +31,17 @@ class Fire(BaseModel):
     gas: int
     temp: List[int]
 
-@app.post("/fire-alarm/add")
-def add_alarm(alarm: Fire):
+@app.post("/fire-alarm/update")
+def update(alarm: Fire):
     alarm_dict = {  'number': alarm.number, 'flame': alarm.flame,
                     'gas': alarm.gas,'temp': alarm.temp,
                     'update_time': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f%z')}
-    
     menu_collection.insert_one(alarm_dict)
-    return "add completed."
 
-
-@app.post("/fire-alarm/update")
-def update(alarm: Fire):
-    pass
+    lst = list(menu_collection.find({'number':alarm.number},{'_id':0})) 
+    lst.sort(key=lambda x:x['update_time']) #sort for delete excess data
+    for i in range(len(lst)-3): #delete in db if more than 3
+        menu_collection.delete_one(lst[i])
+    
+    return "update completed."
+    
