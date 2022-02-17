3d2255b67327cf77db5ec46aba8f31d4cb8a1744
ParnThanatibordee <parn8946@gmail.com>
2022-02-17 09:09:51 +0700

    get_fire_record

--- backend-firealarm.py
+++ backend-firealarm.py
@@ -31,3 +31,16 @@ class Fire(BaseModel):
     gas: List[int]
     fire: List[int]
     temp: int
+
+
+@app.get("/fire-alarm/get-record")
+def get_fire_record():
+    room = db['record_avg'].find({})
+    result = []
+    print('room:', room)
+    for r in room:
+        result.append(
+            {'fire': sum(r['flame']) / len(r['flame']), 'temp': sum(r['temp']) / len(r['temp']), 'ref_temp': 50, 'gas': r['gas'],
+             'ref_gas': 2000})  # default ref temp 50-58, ref gas 2000-5000
+        print(r)
+    return {'Room': result}  # result
