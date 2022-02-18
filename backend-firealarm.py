7e322111432ea4ebe0da2a618f52a8db34039258
Kop <copter1144@hotmail.com>
2022-02-18 19:09:57 +0700

    nothing

--- backend-firealarm.py
+++ backend-firealarm.py
@@ -40,10 +40,11 @@ class Alarm(BaseModel):
 @app.get("/fire-alarm/alarm") #get-hardware
 def alarm():
     lst = list(avg_collection.find({'number':1},{'_id':0})) #search number:1
+    ref = configure_collection.find_one({'number':1},{'_id':0})
     if(len(lst) == 1):
-        flame = 1 if sum(lst[0]['flame']) >= 100 else 0
-        gas = 1 if lst[0]['gas'] >= 2000 else 0
-        temp = 1 if sum(lst[0]['temp']) >= 58 else 0
+        flame = 1 if sum(lst[0]['flame']) > ref['ref_flame'] else 0
+        gas = 1 if lst[0]['gas'] > ref['ref_gas'] else 0
+        temp = 1 if sum(lst[0]['temp']) > ref['ref_temp'] else 0
         return { 'flame': flame, 'gas':gas, 'temp':temp }
     else:
         raise HTTPException(404, "Not have data of this number.")
