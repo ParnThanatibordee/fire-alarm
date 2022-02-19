9e1eb5b95007a50659d9e8cad2fc129edd4ee443
TaninDean <reiodean.dean@gmail.com>
2022-02-19 13:50:26 +0700

    something

--- fire-alarm-back-end/src/components/Card.js
+++ fire-alarm-back-end/src/components/Card.js
@@ -1,31 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Popup from './Popup';
 import PopupDetail from './PopupDetail';
 import PopUpSetting from './PopUpSetting';
 
 
-export default function Card() {
-    const [buttonPopup, setButtonPopup] = useState(false);
-    const [settingPopup, setSettingPopup] = useState(false);
+export default function Card({ state }) {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [settingPopup, setSettingPopup] = useState(false);
 
   return (
     <div className="dashboardcard">
-        <div className="circle" />
-        <br/>
-        <p>Place</p>
-        <br/>
-        <p>Status</p>
-        <br/>
-        <Button onClick={() => setButtonPopup(true)}>Detail</Button>
-        <Button onClick={() => setSettingPopup(true)}>Setting</Button>
-        <Popup trigger={buttonPopup} closePopup={() => setButtonPopup(false)}>
-          <PopupDetail/>
-        </Popup>
-        <Popup trigger={settingPopup} closePopup={() => setSettingPopup(false)}>
-          <PopUpSetting/>
-        </Popup>
-      </div>
+      <div className="circle" />
+      <br />
+      <p>Place</p>
+      <br />
+      <p>Status</p>
+      <br />
+      <Button onClick={() => setButtonPopup(true)}>Detail</Button>
+      <Button onClick={() => setSettingPopup(true)}>Setting</Button>
+      <Popup trigger={buttonPopup} closePopup={() => setButtonPopup(false)}>
+        <PopupDetail />
+      </Popup>
+      <Popup trigger={settingPopup} closePopup={() => setSettingPopup(false)}>
+        <PopUpSetting noti={state} />
+      </Popup>
+    </div>
   )
 }
--- fire-alarm-back-end/src/components/PopUpSetting.js
+++ fire-alarm-back-end/src/components/PopUpSetting.js
@@ -5,37 +5,41 @@ import GasSwitchButton from "./GasSwitchButton";
 import TempSwitchButton from "./TempSwitchButton";
 import { Form } from "react-bootstrap";
 
-export default function PopUpSetting() {
+export default function PopUpSetting({ noti }) {
   const handleSetting = (e) => {
     e.preventDefault();
 
-    const {place, reftemp, refgas} = e.target.element;
+    const { place, reftemp, refgas } = e.target.element;
 
     fetch('', {
-        method: "POST",
-        header: {'Content-Type': 'application/json'},
-        data : JSON.stringify(place.value, reftemp.value, refgas.value)
+      method: "POST",
+      header: { 'Content-Type': 'application/json' },
+      data: JSON.stringify(place.value, reftemp.value, refgas.value)
     });
-}
+  }
   return (
     <div>
       <p>Fire Alarm Setting</p>
       <p>
-        Line Notification <SwitchButton bool={false} />
+        Line Notification <SwitchButton bool={noti.linenoti} />
       </p>
       <p>
-        Fire <FireSwitchButton bool={false} />
+        Fire <FireSwitchButton bool={noti.firenoyi} />
       </p>
       <p>
-        Gas <GasSwitchButton bool={false} />
+        Gas <GasSwitchButton bool={noti.gasnoti} />
       </p>
       <p>
-        Temp <TempSwitchButton bool={false} />
+        Temp <TempSwitchButton bool={noti.tempnoti} />
       </p>
       <Form>
         <Form.Group className="mb-3" >
           <Form.Label>Place Name</Form.Label>
-          <Form.Control type="place" id ="palce" placeholder="Place Name" />
+          <Form.Control type="place" id="palce" placeholder="Place Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Form.Label>Line Token</Form.Label>
+          <Form.Control type="linetoken" id="linetoken" placeholder="Line Token" />
         </Form.Group>
         <Form.Group className="mb-3" >
           <Form.Label>Reference Temperature</Form.Label>
